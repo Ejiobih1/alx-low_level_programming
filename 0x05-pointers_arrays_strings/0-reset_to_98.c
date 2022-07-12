bacb84b9f5f11a6a466a221ebe386361b11297a4
Ejiobih1 <cjr200099@gmail.com>
2022-07-12 03:18:02 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,11 @@
 #include "main.h"
 
 /**
- * Project : 0. 98 Battery st.
- * Programmer: Mehdi Zaabi
- * For Holberton School 2020
- */
-
-#include "main.h"
-
-/**
  * reset_to_98 - resets value of pointer to 98
+ * updates the value it points to to 98
+ *
  * @n: pointer to reset to 98
  *
- * Return: void
  */
 void reset_to_98(int *n)
 {
